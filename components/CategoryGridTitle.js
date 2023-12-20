865df9da922cd908d850868c8fa2d176d9be9f20
Alex Chukrii <chukrii.alex@gmail.com>
2023-12-20 23:55:18 +0100

    Added basic styles for a category

--- components/CategoryGridTitle.js
+++ components/CategoryGridTitle.js
@@ -1,17 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function CategoryGridTitle({ title, color }) {
   return (
-    <View style={styles.category}>
-      <Text>{title}</Text>
+    <View style={styles.outer}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.pressed : null,
+        ]}
+      >
+        <View style={[{ backgroundColor: color }, styles.inner]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  category: {
+  outer: {
     flex: 1,
     height: 150,
+    
+  },
+  button: {
+    flex: 1,
+  },
+  inner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.3,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
 
